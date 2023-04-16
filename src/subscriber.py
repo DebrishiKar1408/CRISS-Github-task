fdac10d416f9add0492c0133b8971e1feec47cc8
DebrishiKar1408 <77243860+DebrishiKar1408@users.noreply.github.com>
2023-04-16 19:50:38 +0530

    Update subscriber.py

--- src/subscriber.py
+++ src/subscriber.py
@@ -3,14 +3,13 @@ import complementary_filter.py as cf
 import rospy
 from std_msgs.msg import Float64MultiArray
 from sensor_msgs.msg import Imu
+p = rospy.Publisher('imu1', Imu, queue_size=100)
 def calback(data):
     quarter = cf.estimate_orientation(a=numpy.array(data[1]),w=numpy.array(data[0])*data[3],t=data[3])
     #This is the required data in imu format (quarternion)
-    p = rospy.Publisher('imu1', Imu, queue_size=100)
     p.publish(quarter)
 def listener():
     rospy.init_node('imu_s_node', anonymous=True)
-
     rospy.Subscriber("imu", Float64MultiArray, callback)
     rospy.spin()
 
