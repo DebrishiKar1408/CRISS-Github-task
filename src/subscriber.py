6edaefa257429c97bc92526d68338e237f3f288f
DebrishiKar1408 <77243860+DebrishiKar1408@users.noreply.github.com>
2023-04-16 18:34:09 +0530

    Update subscriber.py

--- src/subscriber.py
+++ src/subscriber.py
@@ -5,8 +5,14 @@ from std_msgs.msg import Float64MultiArray
 from sensor_msgs.msg import Imu
 def calback(data):
     quarter = cf.estimate_orientation(a=numpy.array(data[1]),w=numpy.array(data[0])*data[3],t=data[3])
+    #This is the required data in imu format (quarternion)
+    p = rospy.Publisher('imu1', Imu, queue_size=100)
+    p.publish(quarter)
 def listener():
     rospy.init_node('imu_s_node', anonymous=True)
 
     rospy.Subscriber("imu", Float64MultiArray, callback)
+    rospy.spin()
 
+if __name__ == '__main__':
+    listener()
